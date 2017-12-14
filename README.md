0738538367c154339d8764e7cdef9c829e68a65d
mamitiana10477 <solofomamitiana0@gmail.com>
2017-12-14 09:49:11 +0300

    Going to write my code cleanly

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # php_tools_n_test
-This is initially done to feel well in the IAM_team
+This is initially done to feel better in the IAM_team
 
 Thinking about replacing this with python_django
--- Test001.php
+++ Test001.php
@@ -7,13 +7,16 @@ require_once 'config_mmt.php';
 // phpinfo();
 
 $prompt = array(
-	"option" => "-T",
+	"option" => "-T",	// this is going to contain all_tests which i_m going to do
+	// "option" => "-h",
 	"values" => array(
 		// "test001"
 		// "test001"
 		// "test_our_tools001"
 		// "test_our_tools002"
-		"test_conn_pg001"
+		// "test_conn_pg001" 		// when i wrote the first time to connect to pg_db with php
+		// "test_formulaire001"		// my first test of php_form
+		"test_ajax001"
 	)
 );
 
@@ -29,6 +32,144 @@ class Person{
 class Our_Tools {
 	function test001(){
 		print "inside our_tools";
+	}
+
+	public static function test_ajax001() {
+		?>
+			<style>
+				label{ width:70px; float:left; }
+				form{ width:320px; }
+				input, textarea{ width:200px;
+				border:1px solid black; float:right; padding:5px; }
+				input[type=submit] { cursor:pointer;
+				background-color:green; color:#FFF; }
+				input[disabled=disabled], input[disabled] {
+				background-color:#d1d1d1; }
+				fieldRow { margin:10px 10px; overflow:hidden; }
+				failed { border: 1px solid red; }
+			</style>
+
+			<body>
+				<h1>Validating form using Ajax</h1>
+				<?php
+				if ( !isset($_POST['url']) ){
+				?>
+					<form class="simpleValidation" method="post" action="Test001.php">
+						<div class="fieldRow">
+							<label>Title *</label>
+							<input type="text" id="title" name="title"
+							class="required" />
+						</div>
+						<div class="fieldRow">
+							<label>Url</label>
+							<input type="text" id="url" name="url"
+							value="http://" />
+						</div>
+						<div class="fieldRow">
+							<label>Labels</label>
+							<input type="text" id="labels" name="labels" />
+						</div>
+						<div class="fieldRow">
+							<label>Text *</label>
+							<textarea id="textarea" class="required"></textarea>
+						</div>
+						<div class="fieldRow">
+							<input type="submit" id="formSubmitter" value="Submit"/>
+							<!-- disabled="disabled"  -->
+
+						</div>
+					</form>
+					<script>
+						var ajaxValidation = function(object){
+						var $this = $(object);
+						var param = $this.attr('name');
+						var value = $this.val();
+						$.get("ajax/inputValidation.php",
+						{'param':param, 'value':value }, function(data) {
+						if(data.status=="OK")
+						validateRequiredInputs();
+						else
+						$this.addClass('failed');
+						},"json");
+						}
+						var validateRequiredInputs = function (){
+						var numberOfMissingInputs = 0;
+						$('.required').each(function(index){
+						var $item = $(this);
+						var itemValue = $item.val();
+						if(itemValue.length) {
+						$item.removeClass('failed');
+						} else {
+						$item.addClass('failed');
+						numberOfMissingInputs++;
+						}
+						});
+						var $submitButton = $('#formSubmitter');
+						if(numberOfMissingInputs > 0){
+						$submitButton.attr("disabled", true);
+						} else {
+						$submitButton.removeAttr('disabled');
+						}
+						}
+
+						$(document).ready(function(){
+						var timerId = 0;
+						$('.required').keyup(function() {
+						clearTimeout (timerId);
+						timerId = setTimeout(function(){
+						ajaxValidation($(this));
+						}, 200);
+						});
+						});
+					</script>
+
+				</body>
+		<?php
+			}else{
+				print "set" . $_POST['url'];
+			}
+	}
+
+	public static function test_formulaire001(){	
+		if ( !isset($_POST['prenom']) ){
+			?>
+				<form method="post" action="Test001.php">
+					<p>
+						<input type="text" name="prenom" />
+						<input type="submit" value="Valider" />
+					</p>
+				</form>
+			<?php
+		}else{
+			print "Le prénom que vous avez entree est: " . $_POST['prenom'];
+		}
+	}
+	public static function usage(){
+		?>
+		<html>
+			<body>
+				<p>
+					<ul>
+						<li style="color: blue">
+							-h
+						</li>
+						Pour que cette page d'aide va s'afficher
+
+						<li style="color: blue">
+							-T test_conn_pg001
+						</li>
+						Pour la faire une test de connection au PostGresSQL
+
+						<li style="color: blue">
+							-T test_formulaire001
+						</li>
+						Pour faire une test de Formulaire avec Php
+					</ul>
+
+				</p>
+			</body>
+		</html>
+		<?php
 	}
 
 	public static function test002(){
@@ -78,6 +219,12 @@ if (
 <?php
 }
 else if (
+	($prompt["option"] == "-h") 
+	// && ($prompt["values"][0] == "test_person" )
+){
+	Our_Tools::usage();
+}
+else if (
 	($prompt["option"] == "-T") 
 	&& ($prompt["values"][0] == "test_person" )
 ){
@@ -102,6 +249,18 @@ else if (
 	&& ($prompt["values"][0] == "test_conn_pg001" )
 ){
 	Our_Tools::test_conn_pg001();
+}
+else if (
+	($prompt["option"] == "-T") 
+	&& ($prompt["values"][0] == "test_formulaire001" )
+){
+	Our_Tools::test_formulaire001();
+}
+else if (
+	($prompt["option"] == "-T") 
+	&& ($prompt["values"][0] == "test_ajax001" )
+){
+	Our_Tools::test_ajax001();
 }
 else{
 	print "non prise en charge";
